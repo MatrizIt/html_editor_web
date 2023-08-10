acfcd6f74ab1bcaf4ec1a9efdd05c32107b4e5e4
Giovanni Zaghi <vanni.freitas@hotmail.com>
2023-08-10 00:17:52 -0300

    fix: new spacing in screen

--- lib/app/features/relatory/widgets/title_content.dart
+++ lib/app/features/relatory/widgets/title_content.dart
@@ -1,7 +1,9 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:reportpad/app/model/teaching_model.dart';
+import 'package:reportpad/app/repository/relatory/i_relatory_repository.dart';
 
 class TitleContent extends StatefulWidget {
   final String title;
@@ -28,73 +30,90 @@ class TitleContent extends StatefulWidget {
 }
 
 class _TitleContentState extends State<TitleContent> {
+  late final IRelatoryRepository repository;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    repository = Modular.get<IRelatoryRepository>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            IconButton(
-                onPressed: widget.changeVisibility,
-                icon: Icon(
-                  widget.isVisible == true
-                      ? Icons.close_outlined
-                      : Icons.refresh_outlined,
-                  color: widget.isVisible == true ? Colors.red : Colors.blue,
-                )),
-            Text(
-              widget.title,
-              style: GoogleFonts.inter(
-                fontWeight: FontWeight.bold,
+        Transform(
+          transform: Matrix4.translationValues(-12.0, 0.0, 0.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                  onPressed: widget.changeVisibility,
+                  icon: Icon(
+                    widget.isVisible != true
+                        ? Icons.close_outlined
+                        : Icons.refresh_outlined,
+                    color: widget.isVisible != true ? Colors.red : Colors.blue,
+                  )),
+              Text(
+                widget.title,
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            PopupMenuButton(
-              child: const Icon(Icons.arrow_drop_down),
-              onSelected: (selectedTeaching) {
-                widget.changeSelectedTeaching(selectedTeaching as int);
-              },
-              itemBuilder: (context) =>
-                  widget.teachings.map<PopupMenuItem>((teaching) {
-                return PopupMenuItem(
-                  value: widget.teachings.indexOf(teaching),
-                  child: Text(
-                    teaching,
-                    style: GoogleFonts.inter(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      color: widget.selectedTeaching ==
-                              widget.teachings.indexOf(teaching)
-                          ? Colors.blueAccent
-                          : Colors.black,
+              const SizedBox(
+                width: 5,
+              ),
+              PopupMenuButton(
+                child: const Icon(Icons.arrow_drop_down),
+                onSelected: (selectedTeaching) {
+                  widget.changeSelectedTeaching(selectedTeaching as int);
+                },
+                itemBuilder: (context) =>
+                    widget.teachings.map<PopupMenuItem>((teaching) {
+                  return PopupMenuItem(
+                    value: widget.teachings.indexOf(teaching),
+                    child: Text(
+                      teaching,
+                      style: GoogleFonts.inter(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        color: widget.selectedTeaching ==
+                                widget.teachings.indexOf(teaching)
+                            ? Colors.blueAccent
+                            : Colors.black,
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            )
-          ],
-        ),
-        AnimatedSwitcher(
-          duration: const Duration(
-            milliseconds: 300,
+                  );
+                }).toList(),
+              )
+            ],
           ),
-          child: widget.isVisible == true
-              ? RichText(
-                  textAlign: TextAlign.justify,
-                  text: TextSpan(
-                    style: GoogleFonts.inter(
-                      fontSize: 14,
-                      color: Colors.black,
-                      height: 1.5,
+        ),
+        Transform(
+          transform: Matrix4.translationValues(0.0, -12.0, 0.0),
+          child: AnimatedSwitcher(
+            duration: const Duration(
+              milliseconds: 300,
+            ),
+            child: widget.isVisible != true
+                ? RichText(
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        color: Colors.black,
+                        height: 1.5,
+                      ),
+                      children: widget.content,
                     ),
-                    children: widget.content,
-                  ),
-                )
-              : const SizedBox.shrink(),
+                  )
+                : const SizedBox.shrink(),
+          ),
         ),
       ],
     );
