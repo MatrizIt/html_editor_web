b4de9eaf3fd6f12bde4d4c7cc3991cfd84ff2f5c
MiguelMarquesIT <miguel.marques@matrizit.com.br>
2023-08-11 20:55:39 -0300

    fix: adjustments in select field

--- lib/app/features/relatory/widgets/app_dropdown_textfield.dart
+++ lib/app/features/relatory/widgets/app_dropdown_textfield.dart
@@ -19,49 +19,58 @@ class AppDropdownTextfield extends StatefulWidget {
 }
 
 class _AppDropdownTextfieldState extends State<AppDropdownTextfield> {
+  final _menuKey = GlobalKey<PopupMenuButtonState>();
+
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Visibility(
-            replacement: Text(
-              widget.hintText,
-              style: GoogleFonts.inter(
-                color: Colors.blueAccent,
-                fontSize: 12,
-              ),
-            ),
-            visible:
-                widget.selectedOption != null && widget.selectedOption != '',
-            child: Text(
-              widget.selectedOption!,
-              style: GoogleFonts.inter(
-                color: Colors.black,
-                fontSize: 12,
-              ),
-            ),
-          ),
-          PopupMenuButton(
-            onSelected: widget.onSelectOption,
-            child: const Icon(Icons.arrow_drop_down),
-            itemBuilder: (context) =>
-                widget.options.map<PopupMenuItem>((option) {
-              return PopupMenuItem(
-                value: option,
-                child: Text(
-                  option,
-                  style: GoogleFonts.inter(
-                    color: widget.selectedOption == option
-                        ? Colors.blueAccent
-                        : Colors.black,
-                  ),
+      child: GestureDetector(
+        onTap: () {
+          final state = _menuKey.currentState;
+          state?.showButtonMenu();
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Visibility(
+              replacement: Text(
+                widget.hintText,
+                style: GoogleFonts.inter(
+                  color: Colors.blueAccent,
+                  fontSize: 12,
                 ),
-              );
-            }).toList(),
-          ),
-        ],
+              ),
+              visible:
+                  widget.selectedOption != null && widget.selectedOption != '',
+              child: Text(
+                widget.selectedOption!,
+                style: GoogleFonts.inter(
+                  color: Colors.black,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            PopupMenuButton(
+              key: _menuKey,
+              onSelected: widget.onSelectOption,
+              child: const Icon(Icons.arrow_drop_down),
+              itemBuilder: (context) =>
+                  widget.options.map<PopupMenuItem>((option) {
+                return PopupMenuItem(
+                  value: option,
+                  child: Text(
+                    option,
+                    style: GoogleFonts.inter(
+                      color: widget.selectedOption == option
+                          ? Colors.blueAccent
+                          : Colors.black,
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
